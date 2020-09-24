9a3df0103601edcbd186e67c5ab97a3730ec4c45
Neha Goel <nehagoel66@gmail.com>
2020-09-24 16:58:09 +0530

    Carausel pics edited

--- src/components/Carausel/Carausel.js
+++ src/components/Carausel/Carausel.js
@@ -3,29 +3,33 @@ import { UncontrolledCarousel, Row, Col } from "reactstrap";
 
 const items = [
   {
-    src:
-      "https://demos.creative-tim.com/argon-design-system-pro/assets/img/faces/team-5.jpg",
+    src: require('../../assets/Image1.jpg'),
     altText: "Slide 1",
     caption: "",
     header: "",
     key: "1",
   },
   {
-    src:
-      "https://demos.creative-tim.com/argon-design-system-pro/assets/img/faces/team-4.jpg",
+    src: require('../../assets/Image2.jpg'),
     altText: "Slide 2",
     caption: "",
     header: "",
     key: "2",
   },
   {
-    src:
-      "https://demos.creative-tim.com/argon-design-system-pro/assets/img/faces/team-3.jpg",
+    src: require('../../assets/Image3.png'),
     altText: "Slide 3",
     caption: "",
     header: "",
     key: "3",
   },
+  {
+    src: require('../../assets/Image4.png'),
+    altText: "Slide 4",
+    caption: "",
+    header: "",
+    key: "4",
+  }
 ];
 
 const Carausel = () => (
