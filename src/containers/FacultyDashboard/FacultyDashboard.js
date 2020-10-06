1088d04d2718783f92ac33d3ac1b3952353b69eb
gurjeetsp <gspadam25@gmail.com>
2020-10-06 16:54:00 +0530

    enter

--- src/containers/FacultyDashboard/FacultyDashboard.js
+++ src/containers/FacultyDashboard/FacultyDashboard.js
@@ -5,7 +5,7 @@ import AddNewResModal from '../../components/AddNewResModal/AddNewResModal';
 import Footer from '../../components/Footer/Footer';
 import AddNote from '../../components/AddNote/AddNote';
 import Calendar from '../../components/Calender/Calendar';
-
+//import Background from '../../components/Background/Background';
 const FacultyDashboard = (props) => {
 
     const onAddRes = () => {
